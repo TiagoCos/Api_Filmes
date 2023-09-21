3ed32588d246f4ba28a668ffb169465a7c760cfd
Your NameTiagoCos <tiagocostasilva27@gmail.com>
2023-09-20 22:08:52 -0300

    criando base com vite

---
+++ movies_lib/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+
+function Navbar() {
+    return (
+        <nav>Navbar
+            <h2>
+                <Link to="/"><BiCameraMovie/>Movies Lib</Link>
+            </h2>
+            <form>
+                <input type="text" placeholder='Busque um filme ' />
+                <button type='Submit'><BiSearchAlt2/></button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar
--- movies_lib/src/main.jsx
+++ movies_lib/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movies.jsx'
+import Search from './pages/Search.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='/Search' element={<Search />} />
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
